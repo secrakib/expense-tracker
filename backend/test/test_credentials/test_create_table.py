a0240ae12db1c7fa0fa05f9e835a028f03f4be5e
rahatpaper11-design <rahatpaper11@gmail.com>
2026-03-03 00:26:24 +0600

    added producion database

--- backend/test/test_credentials/test_create_table.py
+++ backend/test/test_credentials/test_create_table.py
@@ -1,3 +1,7 @@
-from backend.src.credentials.create_table import create_table
+#from backend.src.credentials.create_table import create_table
+from backend.src.feature.create_table import create_table
+from backend.database.globals import location 
+create_table(location)
 
-create_table('backend/test/test_database/database.db')
+
+
